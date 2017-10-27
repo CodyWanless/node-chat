d2b25d516a05484e8e61c79b895d7cd087d8a49c
Cody Wanless <codywanless@Codys-MacBook-Pro.local>
2017-10-27 16:49:26 -0500

    Fix port

--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${3000}`);
+    console.log(`Server is up on port ${port}`);
 });
